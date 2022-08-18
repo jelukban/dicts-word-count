b797b9f60b2bb1ac1c22e6568d59ab3ac9567936
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-08-17 18:48:08 -0700

    Made tokenize function to to take in an input file

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,27 @@
 """Count words in file."""
 
 
-# put your code here.
+def tokenize(filename):
+  """ Takes in a file and returns a list of each word in the text file
+  """
+  input_text = open(filename)
+  list_of_words = []
+
+  for line in input_text:
+    words = line.rstrip().split(' ')
+
+    for word in words:
+      if not word:
+        continue
+
+      if word[-1] in ",./]}!?'[{(-":
+        word = word[:-1]
+      elif word[0] in ",./]}!?'[{(-":
+        word = word[1:]
+
+      list_of_words.append(word)
+
+  input_text.close()
+  return list_of_words
+
+# print(tokenize("twain.txt"))
